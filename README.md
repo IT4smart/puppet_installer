231700396f57841d3399a1d2d8c9e135db4ed1b3
hack3d <hack3d.dev@googlemail.com>
2024-02-03 20:05:22 +0100

    Add powershell script to install puppet agent

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # puppet_installer
-Installer for puppet agent
+Installer for puppet agent on windows
+
+## Example
+`.\install_puppet_agent.ps1 -puppet_master_server "your_puppet_master_server" -puppet_ca_server "your_puppet_ca_server" -puppet_agent_certname "your_puppet_agent_certname" -puppet_agent_environment "your_puppet_agent_environment"`
+
+
---
+++ install_puppet_agent.ps1
@@ -0,0 +1,29 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$puppet_master_server,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$puppet_ca_server,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$puppet_agent_certname,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$puppet_agent_environment,
+    
+    [string]$puppet_agent_startup_mode = "Automatic"
+)
+
+# Define variables
+$PuppetVersion = "7.14.0"
+$PuppetInstallerUrl = "https://downloads.puppet.com/windows/puppet7/puppet-agent-$PuppetVersion-x64.msi"
+
+# Download Puppet agent MSI installer
+Write-Host "Downloading Puppet agent installer..."
+Invoke-WebRequest -Uri $PuppetInstallerUrl -OutFile "puppet-agent.msi"
+
+# Install Puppet agent
+Write-Host "Installing Puppet agent..."
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /norestart /i puppet-agent.msi PUPPET_MASTER_SERVER=$puppet_master_server PUPPET_CA_SERVER=$puppet_ca_server PUPPET_AGENT_CERTNAME=$puppet_agent_certname PUPPET_AGENT_ENVIRONMENT=$puppet_agent_environment PUPPET_AGENT_STARTUP_MODE=$puppet_agent_startup_mode" -Wait
+
+Write-Host "Puppet agent installation and configuration completed."

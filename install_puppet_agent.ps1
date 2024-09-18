368a53f7830ff0b938a7f828f3acc0749f6ae732
hack3d <hack3d.dev@googlemail.com>
2024-09-18 20:39:00 +0200

    Add linting to powershell script

--- install_puppet_agent.ps1
+++ install_puppet_agent.ps1
@@ -19,15 +19,16 @@ $PuppetVersion = "7.14.0"
 $PuppetInstallerUrl = "https://downloads.puppet.com/windows/puppet7/puppet-agent-$PuppetVersion-x64.msi"
 $PuppetAgentCertname = $puppet_agent_certname.ToLower()
 $PuppetAgentEnvironment = $puppet_agent_environment.ToLower()
+$localPuppetPath = "$env:TEMP\puppet-agent.msi"
 
 
 # Download Puppet agent MSI installer
 Write-Host "Downloading Puppet agent installer..."
-Invoke-WebRequest -Uri $PuppetInstallerUrl -OutFile "puppet-agent.msi"
+Invoke-WebRequest -Uri $PuppetInstallerUrl -OutFile $localPuppetPath
 
 # Install Puppet agent
 Write-Host "Installing Puppet agent..."
-$ret = (Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /norestart /log install.txt /i puppet-agent.msi PUPPET_MASTER_SERVER=$puppet_master_server PUPPET_CA_SERVER=$puppet_ca_server PUPPET_AGENT_CERTNAME=$PuppetAgentCertname PUPPET_AGENT_ENVIRONMENT=$PuppetAgentEnvironment PUPPET_AGENT_STARTUP_MODE=$puppet_agent_startup_mode" -Wait -Passthru).ExitCode
+$ret = (Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /norestart /log install.txt /i $localPuppetPath PUPPET_MASTER_SERVER=$puppet_master_server PUPPET_CA_SERVER=$puppet_ca_server PUPPET_AGENT_CERTNAME=$PuppetAgentCertname PUPPET_AGENT_ENVIRONMENT=$PuppetAgentEnvironment PUPPET_AGENT_STARTUP_MODE=$puppet_agent_startup_mode" -Wait -Passthru).ExitCode
 
 if ($ret -eq 0) {
   echo "OK"
